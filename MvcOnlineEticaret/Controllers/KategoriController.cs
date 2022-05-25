f7262fa7caf044246a2f1a5e1e10899749354441
ilker yilmaz <ilkery549@gmail.com>
2022-05-25 23:45:46 +0300

    Category listing with toList method

--- MvcOnlineEticaret/Controllers/KategoriController.cs
+++ MvcOnlineEticaret/Controllers/KategoriController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcOnlineEticaret.Models.Sınıflar;
 
 namespace MvcOnlineEticaret.Controllers
 {
     public class KategoriController : Controller
     {
         // GET: Kategori
+        Context c = new Context();
         public ActionResult Index()
         {
-            return View();
+            var degerler = c.Kategoris.ToList();
+            return View(degerler);
         }
     }
 }

2e5d706e10b2376cd0a6ee534590f4cf56d17df7
ilker yilmaz <ilkery549@gmail.com>
2022-05-27 00:12:44 +0300

    Urun Index Page

---
+++ MvcOnlineEticaret/Controllers/UrunController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineEticaret.Models.Sınıflar;
+
+namespace MvcOnlineEticaret.Controllers
+{
+    public class UrunController : Controller
+    {
+        // GET: Urun
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var urunler = c.Uruns.ToList();
+            return View(urunler);
+        }
+    }
+}

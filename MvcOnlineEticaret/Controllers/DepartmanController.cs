912a3398a48dbc0657765bc9bdd7329235df63b6
ilker yilmaz <ilkery549@gmail.com>
2022-05-27 10:48:21 +0300

    departman Index

---
+++ MvcOnlineEticaret/Controllers/DepartmanController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineEticaret.Models.Sınıflar;
+
+namespace MvcOnlineEticaret.Controllers
+{
+    public class DepartmanController : Controller
+    {
+        // GET: Departman
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var degerler = c.Departmans.ToList();
+            return View(degerler);
+        }
+    }
+}

8e7fe89bb9fb6da4bdb43728e95c0eb7c5d2a2f1
ilker yilmaz <ilkery549@gmail.com>
2022-05-28 17:38:46 +0300

    Personel Controller ve Index

---
+++ MvcOnlineEticaret/Controllers/PersonelController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineEticaret.Models.Sınıflar;
+
+namespace MvcOnlineEticaret.Controllers
+{
+    public class PersonelController : Controller
+    {
+        // GET: Personel
+        Context c = new Context(); //yeni bir nesne üretiyoruz.
+        public ActionResult Index()
+        {
+            var degerler = c.Personels.ToList();
+            return View(degerler);
+        }
+    }
+}

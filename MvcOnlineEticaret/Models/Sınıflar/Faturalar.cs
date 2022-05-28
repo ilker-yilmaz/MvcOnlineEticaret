d66b94b76e76538ed7305dca4965d8d4a483ddf6
ilker yilmaz <ilkery549@gmail.com>
2022-05-29 01:53:53 +0300

    update database fatura

--- MvcOnlineEticaret/Models/Sınıflar/Faturalar.cs
+++ MvcOnlineEticaret/Models/Sınıflar/Faturalar.cs
@@ -24,7 +24,10 @@ namespace MvcOnlineEticaret.Models.Sınıflar
         [Column(TypeName = "Varchar")]
         [StringLength(60)]
         public string VergiDairesi { get; set; }
-        public DateTime Saat { get; set; }
+
+        [Column(TypeName = "char")]
+        [StringLength(5)]
+        public string Saat { get; set; }
 
         [Column(TypeName = "Varchar")]
         [StringLength(30)]
@@ -33,6 +36,9 @@ namespace MvcOnlineEticaret.Models.Sınıflar
         [Column(TypeName = "Varchar")]
         [StringLength(30)]
         public string TeslimAlan { get; set; }
+
+        public decimal Toplam { get; set; }
+
         public ICollection<FaturaKalem> FatureKalems { get; set; }
 
     }

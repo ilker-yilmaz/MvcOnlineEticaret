9d9978008fc18ba8ccec7c8a9c46189c9018fcc1
ilker yilmaz <ilkery549@gmail.com>
2022-05-28 21:19:41 +0300

    Satış Index

---
+++ MvcOnlineEticaret/Controllers/SatisController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineEticaret.Models.Sınıflar;
+
+namespace MvcOnlineEticaret.Controllers
+{
+    public class SatisController : Controller
+    {
+        // GET: Satis
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var degerler = c.SatisHarekets.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult YeniSatis()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniSatis(SatisHareket s)
+        {
+            c.SatisHarekets.Add(s);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}

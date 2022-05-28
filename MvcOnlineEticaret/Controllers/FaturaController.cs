1e9765bc52472b1a0bf62273f2971ae62e329bf3
ilker yilmaz <ilkery549@gmail.com>
2022-05-29 01:43:29 +0300

    FaturaController added

---
+++ MvcOnlineEticaret/Controllers/FaturaController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineEticaret.Models.Sınıflar;
+
+namespace MvcOnlineEticaret.Controllers
+{
+    public class FaturaController : Controller
+    {
+        // GET: Fatura
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var liste = c.Faturalars.ToList();
+            return View(liste);
+        }
+    }
+}

953a04ad1ee35805e09a995f5ac78440ae6a9fb1
ilker yilmaz <ilkery549@gmail.com>
2022-05-28 19:39:53 +0300

    Yeni Personel Ekleme

--- MvcOnlineEticaret/Controllers/PersonelController.cs
+++ MvcOnlineEticaret/Controllers/PersonelController.cs
@@ -16,5 +16,24 @@ namespace MvcOnlineEticaret.Controllers
             var degerler = c.Personels.ToList();
             return View(degerler);
         }
+        [HttpGet]
+        public ActionResult PersonelEkle()
+        {
+            List<SelectListItem> deger1 = (from x in c.Departmans.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.DepartmanAd,
+                                               Value = x.Departmanid.ToString()
+                                           }).ToList();
+            ViewBag.dgr1 = deger1;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult PersonelEkle(Personel p)
+        {
+            c.Personels.Add(p);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }

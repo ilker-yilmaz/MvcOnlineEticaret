3adb1afeebbdadbec8128bf0744f3750afbf4ba7
ilker yilmaz <ilkery549@gmail.com>
2022-05-28 20:04:40 +0300

    Personel Detayları

--- MvcOnlineEticaret/Controllers/PersonelController.cs
+++ MvcOnlineEticaret/Controllers/PersonelController.cs
@@ -35,5 +35,27 @@ namespace MvcOnlineEticaret.Controllers
             c.SaveChanges();
             return RedirectToAction("Index");
         }
+        public ActionResult PersonelGetir(int id)
+        {
+            List<SelectListItem> deger1 = (from x in c.Departmans.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.DepartmanAd,
+                                               Value = x.Departmanid.ToString()
+                                           }).ToList();
+            ViewBag.dgr1 = deger1;
+            var prs = c.Personels.Find(id);
+            return View("PersonelGetir",prs);
+        }
+        public ActionResult PersonelGuncelle(Personel p)
+        {
+            var prsn = c.Personels.Find(p.Personelid);
+            prsn.PersonelAd = p.PersonelAd;
+            prsn.PersonelSoyad = p.PersonelSoyad;
+            prsn.PersonelGorsel = p.PersonelGorsel;
+            prsn.Departmanid = p.Departmanid;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }

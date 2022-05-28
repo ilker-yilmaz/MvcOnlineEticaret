290a319725021294e6270e2b70462a2af98444e1
ilker yilmaz <ilkery549@gmail.com>
2022-05-28 22:06:33 +0300

    Yeni Satış Yapma

--- MvcOnlineEticaret/Controllers/SatisController.cs
+++ MvcOnlineEticaret/Controllers/SatisController.cs
@@ -19,11 +19,36 @@ namespace MvcOnlineEticaret.Controllers
         [HttpGet]
         public ActionResult YeniSatis()
         {
+            List<SelectListItem> deger1 = (from x in c.Uruns.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.UrunAd,
+                                               Value = x.Urunid.ToString()
+                                           }).ToList();
+
+
+            List<SelectListItem> deger2 = (from x in c.Carilers.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.CariAd + " " + x.CariSoyad,
+                                               Value = x.Cariid.ToString()
+                                           }).ToList();
+
+            List<SelectListItem> deger3 = (from x in c.Personels.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.PersonelAd + " " + x.PersonelSoyad,
+                                               Value = x.Personelid.ToString()
+                                           }).ToList();
+            ViewBag.dgr1 = deger1;
+            ViewBag.dgr2 = deger2;
+            ViewBag.dgr3 = deger3;
             return View();
         }
         [HttpPost]
         public ActionResult YeniSatis(SatisHareket s)
         {
+            s.Tarih = DateTime.Parse(DateTime.Now.ToShortDateString()); //şimdiki tarihi alır
             c.SatisHarekets.Add(s);
             c.SaveChanges();
             return RedirectToAction("Index");

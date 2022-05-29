49e399b696f2d551863e4caaed9843dbeb4ee03b
ilker yilmaz <ilkery549@gmail.com>
2022-05-29 12:19:15 +0300

    FaturaKalem update-database

--- MvcOnlineEticaret/Models/Sınıflar/FaturaKalem.cs
+++ MvcOnlineEticaret/Models/Sınıflar/FaturaKalem.cs
@@ -18,7 +18,8 @@ namespace MvcOnlineEticaret.Models.Sınıflar
         public int Miktar { get; set; }
         public decimal BirimFiyat { get; set; }
         public decimal Tutar { get; set; }
-        public Faturalar Faturalar { get; set; }
+        public int Faturaid { get; set; }
+        public virtual Faturalar Faturalar { get; set; }
 
     }
 }

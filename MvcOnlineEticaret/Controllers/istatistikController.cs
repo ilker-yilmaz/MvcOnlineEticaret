142fe09efbf330c4d6685fe60e5d96b267a98dd3
ilker yilmaz <ilkery549@gmail.com>
2022-05-29 23:59:07 +0300

    istatistik Index

---
+++ MvcOnlineEticaret/Controllers/istatistikController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOnlineEticaret.Controllers
+{
+    public class istatistikController : Controller
+    {
+        // GET: istatistik
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

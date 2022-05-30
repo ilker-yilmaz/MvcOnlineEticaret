ba3b823912490523355014f70648f32a9d5fcfd0
ilker yilmaz <ilkery549@gmail.com>
2022-05-30 22:44:53 +0300

    ürün görsellerini çekme

--- MvcOnlineEticaret/Controllers/GaleriController.cs
+++ MvcOnlineEticaret/Controllers/GaleriController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcOnlineEticaret.Models.Sınıflar;
 
 namespace MvcOnlineEticaret.Controllers
 {
     public class GaleriController : Controller
     {
         // GET: Galeri
+        Context c = new Context();
         public ActionResult Index()
         {
-            return View();
+            var degerler = c.Uruns.ToList();
+            return View(degerler);
         }
     }
 }

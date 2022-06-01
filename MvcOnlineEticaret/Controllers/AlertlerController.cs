dbcb1bee138fe904b9518d49b3418c450b2f681c
ilker yilmaz <ilkery549@gmail.com>
2022-06-01 23:38:39 +0300

    sweet alert usage

---
+++ MvcOnlineEticaret/Controllers/AlertlerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOnlineEticaret.Controllers
+{
+    public class AlertlerController : Controller
+    {
+        // GET: Alertler
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

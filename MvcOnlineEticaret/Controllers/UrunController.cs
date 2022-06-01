a6dc0721a8e78c26163c54cb81d0e3b1b1509433
ilker yilmaz <ilkery549@gmail.com>
2022-06-02 00:41:38 +0300

    ürünlerde satış yapma, ürün satış işlemi

--- MvcOnlineEticaret/Controllers/UrunController.cs
+++ MvcOnlineEticaret/Controllers/UrunController.cs
@@ -80,5 +80,31 @@ namespace MvcOnlineEticaret.Controllers
             var degerler = c.Uruns.ToList();
             return View(degerler);
         }
+
+        [HttpGet]
+        public ActionResult SatisYap(int id)
+        {
+            List<SelectListItem> deger3 = (from x in c.Personels.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.PersonelAd + " " + x.PersonelSoyad,
+                                               Value = x.Personelid.ToString()
+                                           }).ToList();
+
+            ViewBag.dgr3 = deger3;
+            var deger1 = c.Uruns.Find(id);
+            ViewBag.dgr1 = deger1.Urunid;
+            ViewBag.dgr2 = deger1.SatisFiyat;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SatisYap(SatisHareket p)
+        {
+            p.Tarih = DateTime.Parse(DateTime.Now.ToShortDateString());
+            c.SatisHarekets.Add(p);
+            c.SaveChanges();
+            return RedirectToAction("Index", "Satis");
+        }
+
     }
 }

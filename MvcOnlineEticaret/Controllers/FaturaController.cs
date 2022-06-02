ed27c73ec09a7d2f45750c3d99888a162948456a
ilker yilmaz <ilkery549@gmail.com>
2022-06-03 00:33:22 +0300

    dinamik faturalar

--- MvcOnlineEticaret/Controllers/FaturaController.cs
+++ MvcOnlineEticaret/Controllers/FaturaController.cs
@@ -66,5 +66,38 @@ namespace MvcOnlineEticaret.Controllers
             c.SaveChanges();
             return RedirectToAction("Index");
         }
+        public ActionResult Dinamik()
+        {
+            Class4 cs = new Class4();
+            cs.deger1 = c.Faturalars.ToList();
+            cs.deger2 = c.FaturaKalems.ToList();
+            return View(cs);
+        }
+        public ActionResult FaturaKaydet(string FaturaSeriNo, string FaturaSıraNo, DateTime Tarih, string VergiDairesi, string Saat, string TeslimEden, string TeslimAlan, string Toplam, FaturaKalem[] kalemler)
+        {
+            Faturalar f = new Faturalar();
+            f.FaturaSeriNo = FaturaSeriNo;
+            f.FaturaSıraNo = FaturaSıraNo;
+            f.Tarih = Tarih;
+            f.VergiDairesi = VergiDairesi;
+            f.Saat = Saat;
+            f.TeslimEden = TeslimEden;
+            f.TeslimAlan = TeslimAlan;
+            f.Toplam = decimal.Parse(Toplam);
+            c.Faturalars.Add(f);
+            foreach (var x in kalemler)
+            {
+                FaturaKalem fk = new FaturaKalem();
+                fk.Aciklama = x.Aciklama;
+                fk.BirimFiyat = x.BirimFiyat;
+                fk.Faturaid = x.FaturaKalemid;
+                fk.Miktar = x.Miktar;
+                fk.Tutar = x.Tutar;
+                c.FaturaKalems.Add(fk);
+            }
+            c.SaveChanges();
+            return Json("İşlem Başarılı", JsonRequestBehavior.AllowGet);
+        }
+
     }
 }
---
+++ MvcOnlineEticaret/Models/Sınıflar/Class4.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineEticaret.Models.Sınıflar
+{
+    public class Class4
+    {
+        public IEnumerable<Faturalar> deger1 { get; set; }
+        public IEnumerable<FaturaKalem> deger2 { get; set; }
+
+    }
+}
